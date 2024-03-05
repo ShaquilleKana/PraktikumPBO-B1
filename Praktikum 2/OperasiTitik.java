abc1ce3d83bda672e6da80987c1301016ea8b451
ShaquilleKana <160566252+ShaquilleKana@users.noreply.github.com>
2024-03-05 11:31:28 +0700

    Add files via upload

---
+++ Praktikum 2/OperasiTitik.java
@@ -0,0 +1,19 @@
+public class OperasiTitik {
+    private void refleksiSumbuX(Titik titik){
+        titik.setOrdinat(-titik.getOrdinat());
+    }
+
+    private void refleksiSumbuY(Titik titik){
+        titik.setAbsis(-titik.getAbsis());
+    }
+
+    public Titik refleksiX(Titik titik){
+        refleksiSumbuX(titik);
+        return titik;
+    }
+
+    public Titik refleksiY(Titik titik){
+        refleksiSumbuY(titik);
+        return titik;
+    }
+}
