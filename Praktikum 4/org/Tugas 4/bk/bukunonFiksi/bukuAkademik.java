84bb88570bce0e722a558414a44aa9dddb438c8d
ShaquilleKana <160566252+ShaquilleKana@users.noreply.github.com>
2024-06-19 00:50:29 +0700

    Add files via upload

---
+++ Praktikum 4/org/Tugas 4/bk/bukunonFiksi/bukuAkademik.java
@@ -0,0 +1,14 @@
+/*
+    Nama        : Mohamad Faisal Rizki
+    NIM         : 24060122130068
+    Nama File   : bukuAkademik.java
+    Deskripsi   : File Class dari bukuAkademik.java
+*/
+
+package bk.bukunonFiksi;
+
+public class bukuAkademik extends bukuNonFiksi{
+    public bukuAkademik(String judul, String penulis, String tahunPenerbit, String subjek, int jumlahHalaman, double harga){
+        super(judul, penulis, tahunPenerbit, subjek, jumlahHalaman, harga);
+    }
+}
